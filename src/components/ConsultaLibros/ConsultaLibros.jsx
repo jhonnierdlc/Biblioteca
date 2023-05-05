4cd75e1c9a53f6ea9a2abb6d93df114c72cc4efe
jhonnierdlc <jhonierg20@gmail.com>
2023-05-05 14:51:32 -0500

    prueba api libros

--- src/components/ConsultaLibros/ConsultaLibros.jsx
+++ src/components/ConsultaLibros/ConsultaLibros.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ConsultarLibro.css";
+import axios from "axios";
+import Home from "../Home/Home";
 
-const ConsultaLibros = () => {
+const ConsultaLibros = ({ libros }) => {
+  const [libro, setlibro] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://64546c7fc18adbbdfeb5f084.mockapi.io/libros/libros")
+      .then((response) => setlibro(response.data));
+  }, []);
+
   return (
-    <div>
-      <div className="consultalibro text-center">
-        <h4>Libros Disponibles</h4>
-        {/* Cards para los libros */}
-        <div className="cardslibros">
-          <div className="card">
-            <img className="card-img-top" src="..." alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
+    <div className="row">
+      <h4>Libros Disponibles</h4>
+      <div className="consultalibro text-center card-group">
+        {!libro
+          ? "Cargando.."
+          : libro.map((libro, index) => {
+              return (
+                <div>
+                  <div className="cardslibros ">
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src={libro.imagen}
+                        alt="Card image cap"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">{libro.titulo}</h5>
+                        <p className="card-text">
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </p>
+                        <a href="#" className="btn btn-primary">
+                          Ver
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 
-const Home = () => {
+const Home = ({ libro }) => {
   return (
     <div className="home">
       <div className="pantallainicio row">
@@ -27,49 +27,60 @@ const Home = () => {
           d="M0,32L40,42.7C80,53,160,75,240,74.7C320,75,400,53,480,69.3C560,85,640,139,720,149.3C800,160,880,128,960,112C1040,96,1120,96,1200,90.7C1280,85,1360,75,1400,69.3L1440,64L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
         ></path>
       </svg>
+
       <div className="algunoslibros row">
         <div className="containerlib col-sm">
-          <div className="item">
-            <h6>Titulo</h6>
-            <img src="" alt="Imagen" />
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-              qui nemo. Enim unde temporibus eveniet est quae quasi dolores
-              voluptatem quibusdam incidunt quos recusandae nemo commodi
-            </p>
+          {!libro
+            ? "Cargando.."
+            : libro.map((libro, index) => {
+                return (
+                  <div className="libro">
+                    <div className="item">
+                      <h6>{libro.titulo}</h6>
+                      <img src="" alt="Imagen" />
+                      <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cumque, qui nemo. Enim unde temporibus eveniet est
+                        quae quasi dolores voluptatem quibusdam incidunt quos
+                        recusandae nemo commodi
+                      </p>
+                    </div>
+                    <div className="item">
+                      <h6>Titulo</h6>
+                      <img src="" alt="Imagen" />
+                      <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cumque, qui nemo. Enim unde temporibus eveniet est
+                        quae quasi dolores voluptatem quibusdam incidunt quos
+                        recusandae nemo commodi
+                      </p>
+                    </div>
+                    <div className="item">
+                      <h6>Titulo</h6>
+                      <img src="" alt="Imagen" />
+                      <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cumque, qui nemo. Enim unde temporibus eveniet est
+                        quae quasi dolores voluptatem quibusdam incidunt quos
+                        recusandae nemo commodi
+                      </p>
+                    </div>
+                    <div className="item">
+                      <h6>Titulo</h6>
+                      <img src="" alt="Imagen" />
+                      <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cumque, qui nemo. Enim unde temporibus eveniet est
+                        quae quasi dolores voluptatem quibusdam incidunt quos
+                        recusandae nemo commodi
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+          <div className="descripcionlib col-sm">
+            <h5>Algunos Libros Disponibles</h5>
           </div>
-          <div className="item">
-            <h6>Titulo</h6>
-            <img src="" alt="Imagen" />
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-              qui nemo. Enim unde temporibus eveniet est quae quasi dolores
-              voluptatem quibusdam incidunt quos recusandae nemo commodi
-            </p>
-          </div>
-          <div className="item">
-            <h6>Titulo</h6>
-            <img src="" alt="Imagen" />
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-              qui nemo. Enim unde temporibus eveniet est quae quasi dolores
-              voluptatem quibusdam incidunt quos recusandae nemo commodi
-            </p>
-          </div>
-          <div className="item">
-            <h6>Titulo</h6>
-            <img src="" alt="Imagen" />
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-              qui nemo. Enim unde temporibus eveniet est quae quasi dolores
-              voluptatem quibusdam incidunt quos recusandae nemo commodi
-            </p>
-          </div>
-        </div>
-
-        <div className="descripcionlib col-sm">
-          <h5>Algunos Libros Disponibles</h5>
-          <p>Libros</p>
         </div>
       </div>
     </div>
